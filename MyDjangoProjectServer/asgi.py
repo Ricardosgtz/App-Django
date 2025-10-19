ed961bffa3eeb834f0b9997e7acabab25f936d2e
Ricardo Santos <ricardosgtz@gmail.com>
2025-10-18 19:56:13 -0600

    Websockets

--- MyDjangoProjectServer/asgi.py
+++ MyDjangoProjectServer/asgi.py
@@ -13,7 +13,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from orders.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurante.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyDjangoProjectServer.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
