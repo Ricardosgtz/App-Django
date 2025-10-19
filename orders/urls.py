66cb1b9dc05c634531152dc1e26440ac47520ad1
Ricardo Santos <ricardosgtz@gmail.com>
2025-10-18 20:46:47 -0600

    ws

--- orders/urls.py
+++ orders/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import create_order, get_orders_by_client, get_order_detail, test_ws
 
 urlpatterns = [
-    path('/test_ws', test_ws),
+    path('/ws/test_ws', test_ws),
     path('', create_order),
     path('/client/<client_id>', get_orders_by_client),
     path('/<order_id>', get_order_detail),
